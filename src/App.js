f7e9521f8efefa469fa5b27c891881a001033b84
Codi <codingmagesty@gmail.com>
2018-12-27 17:09:21 +0300

    Adding Particles effect

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
 import './App.css';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+const ParticlesOptions = {
+	    particles: {
+	        number: {
+	            value: 150,
+              density: {
+                enable: true,
+                value_area: 800
+              }
+	        }
+	    }
+	}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+        params= {ParticlesOptions}/>
         <Navigation />
         <Logo />
         <Rank />
