bb3f2706fa4006a393302b7ccc7a8d29e8e4a5d7
Codi <codingmagesty@gmail.com>
2018-12-28 01:06:28 +0300

    Adding blue boxes around faces

--- src/App.js
+++ src/App.js
@@ -30,8 +30,27 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifyFace.left_col * width,
+      topRow: clarifyFace.top_row * height,
+      rightCol: width - (clarifyFace.right_col * width),
+      bottomRow: height - (clarifyFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -41,16 +60,9 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      // do something with response
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-      console.log(err);
-    }
-  );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render() {
@@ -64,7 +76,7 @@ class App extends Component {
         <ImageLinkForm
           onInputChange = {this.onInputChange}
           onButtonSubmit = {this.onButtonSubmit}/>
-        <FaceRecognition imageUrl = { this.state.imageUrl }/>
+        <FaceRecognition imageUrl = { this.state.imageUrl } box = { this.state.box }/>
       </div>
     );
   }
